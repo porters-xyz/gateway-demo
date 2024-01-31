05c337e8caba22defd3eb21a94391320fc0dd123
memosys <82053242+memosys@users.noreply.github.com>
2024-01-31 19:29:27 +0530

    wip: added validate & create calls with buttons on frontend

--- web-portal/backend/src/main.ts
+++ web-portal/backend/src/main.ts
@@ -8,6 +8,11 @@ async function bootstrap() {
   // prisma exception filter
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
--- web-portal/frontend/app/login/page.tsx
+++ web-portal/frontend/app/login/page.tsx
@@ -1,25 +1,64 @@
-import { Container, Stack, Button, TextInput } from "@mantine/core";
+"use client";
+import { Container, Button, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import axios from "axios";
+
+const validateTenant = async (key: string) => {
+  try {
+    const response = await axios.get(
+      "http://localhost:4000/tenant/validate/" + key,
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error making GET request:", error.message);
+  }
+};
+
+const createTenant = async () => {
+  try {
+    const response = await axios.get("http://localhost:4000/tenant/create");
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error making POST request:", error.message);
+  }
+};
 
 export default function Login() {
-  return (
-    <Container
-      style={{
-        height: "100vh",
-        width: "50vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        gap: 8,
-      }}
-    >
-      <TextInput label="Secret Key" placeholder="Secret" width={"full"} />
-      <Button variant="filled" fullWidth>
-        Login
-      </Button>
+  const form = useForm({
+    initialValues: {
+      key: "",
+    },
+    validate: {
+      key: (value) => (value.length === 16 ? null : "Invalid Key"),
+    },
+  });
 
-      <Button variant="light" fullWidth>
-        Create Key
-      </Button>
-    </Container>
+  return (
+    <form onSubmit={form.onSubmit((values) => validateTenant(values.key))}>
+      <Container
+        style={{
+          height: "100vh",
+          width: "50vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          gap: 8,
+        }}
+      >
+        <TextInput
+          label="Secret Key"
+          placeholder="Secret"
+          width={"full"}
+          {...form.getInputProps("key")}
+        />
+        <Button type="submit" variant="filled" fullWidth>
+          Login
+        </Button>
+
+        <Button variant="light" fullWidth onClick={() => createTenant()}>
+          Create Key
+        </Button>
+      </Container>
+    </form>
   );
 }

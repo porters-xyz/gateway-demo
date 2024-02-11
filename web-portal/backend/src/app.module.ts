098f9cd42b105ecaf838f2b1114d95ee21488c25
memosys <82053242+memosys@users.noreply.github.com>
2024-02-12 00:40:11 +0530

    Removed root nodejs clutter, migrated to sha256, justfile commands
    This PR aims to simplify and clean the repo, decentralise js repos, migrate to SHA256 for hashing as discussed!
    
    todo: just migrate doesn't work unless db url provided directly via string!

--- web-portal/backend/src/app.module.ts
+++ web-portal/backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
 import { CustomPrismaModule } from 'nestjs-prisma';
-import { PrismaClient } from '../../../.generated/client';
+import { PrismaClient } from '@/.generated/client';
 import { TenantModule } from './tenant/tenant.module';
 import { AppController } from './app.controller';
 import { AuthkeysModule } from './authkeys/authkeys.module';
@@ -13,6 +13,9 @@ import { AuthkeysModule } from './authkeys/authkeys.module';
       name: 'Postgres',
       client: new PrismaClient(),
       isGlobal: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['@/.env', '.env.local'],
     }),
     TenantModule,
     AuthkeysModule,

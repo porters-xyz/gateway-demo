cf00382905070ad63ba7c09c7afaec2a59a24df2
memosys <82053242+memosys@users.noreply.github.com>
2024-03-16 12:08:10 +0530

    added favicon

--- web-portal/frontend/app/layout.tsx
+++ web-portal/frontend/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <head>
         <ColorSchemeScript />
       </head>

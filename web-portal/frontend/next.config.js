ce51008f35ff54999318ca41870fc6492dac4de0
memosys <82053242+memosys@users.noreply.github.com>
2024-03-17 18:34:38 +0530

    test

--- web-portal/frontend/next.config.js
+++ web-portal/frontend/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const apiUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
 const nextConfig = {
   output: "standalone",
+  env: {
+    apiUrl: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty");
     config.resolve.fallback = { fs: false, net: false, tls: false };

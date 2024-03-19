450cd355757837d4e9d81076fe48e968953e8b4b
memosys <82053242+memosys@users.noreply.github.com>
2024-03-19 12:51:12 +0530

    added some seeds for rules

--- services/postgres/seed.ts
+++ services/postgres/seed.ts
@@ -1,20 +1,61 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../web-portal/backend/.generated/client";
 
 const prisma = new PrismaClient();
 
-// TODO: Initial seed data
+async function main() {
+  try {
+    await prisma.ruleType.createMany({
+      data: [
+        {
+          name: "Allowed Origins",
+          isEditable: true,
+          isMultiple: true,
+          validationType: "regex",
+          validationValue: `https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()!@:%_+.~#?&/\\//=]*)`,
+        },
+        {
+          name: "Approved Chains",
+          isEditable: true,
+          isMultiple: true,
+          validationType: "function",
+          validationValue: "Array.isArray(value)",
+        },
+        {
+          name: "Secret Key",
+          isEditable: false,
+          isMultiple: false,
+          validationType: "function",
+          validationValue: 'typeof value === "string" && value.length === 32',
+        },
+        {
+          name: "Allowed UserAgents",
+          isEditable: true,
+          isMultiple: true,
+          validationType: "regex",
+          validationValue: "*",
+        },
+        {
+          name: "Allowed Contracts",
+          isEditable: true,
+          isMultiple: true,
+          validationType: "function",
+          validationValue:
+            "values.map((value) => value.isNull || /^0x[a-fA-F0-9]{40}$/.test(value))",
+        },
+        {
+          name: "Allowed Methods",
+          isEditable: true,
+          isMultiple: true,
+          validationType: "function",
+          validationValue: "Array.isArray(value)",
+        },
+      ],
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
 
-// async function main() {
-//   await prisma.user.create({
-//     data: {
-//       name: 'Ham Burger',
-//       email: 'ham@burger.dev',
-//     },
-//   });
-// }
-
-// main()
-//   .catch((e) => console.error(e))
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+main();

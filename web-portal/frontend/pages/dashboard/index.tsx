32a23b97a11959887eb2b77f519f738af6503aa1
memosys <82053242+memosys@users.noreply.github.com>
2024-03-26 00:58:41 +0530

    Update index.tsx

--- web-portal/frontend/pages/dashboard/index.tsx
+++ web-portal/frontend/pages/dashboard/index.tsx
@@ -1,10 +1,8 @@
 import AppList from "@frontend/components/dashboard/applist";
 import { Stack } from "@mantine/core";
-import { useRouter } from "next/navigation";
 import { useUserApps } from "@frontend/utils/hooks";
 import _ from "lodash";
 import DashboardLayout from "@frontend/components/dashboard/layout";
-import { useEffect } from "react";
 import Insights from "@frontend/components/dashboard/insights";
 import CreateAppModal from "@frontend/components/dashboard/createAppModal";
 import { useSession } from "@frontend/utils/hooks";

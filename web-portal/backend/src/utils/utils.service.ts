f5413ef5d27e34384fbfc6a7bf549c35f4015ebe
memosys <82053242+memosys@users.noreply.github.com>
2024-03-29 15:02:58 +0530

    get chains end point

---
+++ web-portal/backend/src/utils/utils.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
+import { CustomPrismaService } from 'nestjs-prisma';
+import { PrismaClient, TransactionType } from '@/.generated/client';
+
+@Injectable()
+export class UtilsService {
+  constructor(
+    @Inject('Postgres')
+    private prisma: CustomPrismaService<PrismaClient>, // <-- Inject the PrismaClient
+  ) {}
+
+  async getChains() {
+    const chains = this.prisma.client.products.findMany();
+
+    if (!chains)
+      throw new HttpException(
+        'No Chains/Services Found',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    return chains;
+  }
+}

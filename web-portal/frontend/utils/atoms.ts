84413e80642825aae277a0a0b90de70ea6af6bea
memosys <82053242+memosys@users.noreply.github.com>
2024-03-29 16:40:16 +0530

    save:progress

--- web-portal/frontend/utils/atoms.ts
+++ web-portal/frontend/utils/atoms.ts
@@ -1,4 +1,5 @@
 import { atom } from "jotai";
-export const sessionAtom = atom({});
+import { ISession } from "./types";
+export const sessionAtom = atom<ISession>({});
 export const appsAtom = atom([]);
 export const endpointsAtoms = atom([]);

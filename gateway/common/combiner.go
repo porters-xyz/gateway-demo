c963dc862f9b155ac4902565b0b35ad19320634f
plor <plor@nihil.wtf>
2024-04-14 18:43:16 -0500

    Testing the job queue

---
+++ gateway/common/combiner.go
@@ -0,0 +1,61 @@
+package common
+
+import (
+    "log"
+)
+
+// rather than be too chatty, combine multiple tasks into one
+// idea here is to eat through the channel combining multiple tasks
+// into a single one which can then be persisted
+type Combinable interface {
+    Runnable
+    Combine(q chan Runnable) // put items back on channel if don't match
+    key() string // combine with others of same key
+    value() int // amount to combine
+    gen() int // allow combiners to mate
+}
+
+type SimpleCombiner struct {
+    SimpleTask
+    keyVal string
+    intVal int
+    genVal int
+}
+
+// Implement Combinable if more complex functions are needed
+func (c *SimpleCombiner) Combine(q chan Runnable) {
+    for i := len(q); i > 0; i-- {
+        task := <- q
+        switch task.(type) {
+        case Combinable:
+            d := task.(Combinable)
+            if c.keyVal == d.key() {
+                c.intVal += d.value()
+                c.genVal = max(c.gen(), d.gen())
+            } else {
+                q <- task
+            }
+        default:
+            q <- task
+        }
+    }
+    // after combining, remove the combinable bits
+    if c.gen() >= 10 {
+        c.run = c.runner()
+        q <- &c.SimpleTask
+    } else {
+        c.genVal++
+        q <- c
+    }
+}
+
+func (c *SimpleCombiner) key() string { return c.keyVal }
+func (c *SimpleCombiner) value() int { return c.intVal }
+func (c *SimpleCombiner) gen() int { return c.genVal }
+// This gets set to the SimpleTask.run
+// Simple version only logs, need to extend to push to redis, etc
+func (c *SimpleCombiner) runner() func() {
+    return func() {
+        log.Printf("combined %s to %d", c.keyVal, c.intVal)
+    }
+}
--- gateway/common/tasks.go
+++ gateway/common/tasks.go
@@ -13,17 +13,6 @@ type Runnable interface {
     Run()
 }
 
-// rather than be too chatty, combine multiple tasks into one
-// idea here is to eat through the channel combining multiple tasks
-// into a single one which can then be persisted
-type Combinable interface {
-    Runnable
-    Combine(q chan Runnable) // put items back on channel if don't match
-    key() string // combine with others of same key
-    value() int // amount to combine
-    runner() func() // override to set runnable
-}
-
 // contains bits necessary to run later
 type SimpleTask struct {
     run func()
@@ -32,11 +21,6 @@ type SimpleTask struct {
 type TaskQueue struct {
     Tasks chan Runnable
     errors chan error
-}
-
-type SimpleCombiner struct {
-    keyVal string
-    intVal int
 }
 
 var q *TaskQueue
@@ -61,7 +45,7 @@ func GetTaskQueue() *TaskQueue {
     return q
 }
 
-func (q *TaskQueue) setupWorkers() {
+func (q *TaskQueue) SetupWorkers() {
     numWorkers := 10 // default
     numWorkersEnv, ok := os.LookupEnv("NUM_WORKERS")
     numWorkersInt, err := strconv.Atoi(numWorkersEnv)
@@ -76,7 +60,7 @@ func (q *TaskQueue) setupWorkers() {
 }
 
 // use this for graceful shutdown
-func (q *TaskQueue) closeQueue() {
+func (q *TaskQueue) CloseQueue() {
     close(q.Tasks)
 }
 
@@ -96,32 +80,4 @@ func worker(q *TaskQueue) {
 // SimpleTask can be extended if needed
 func (t *SimpleTask) Run() {
     t.run()
-}
-
-// Implement Combinable if more complex functions are needed
-func (c *SimpleCombiner) Combine(q chan Runnable) {
-    for i := len(q); i > 0; i-- {
-        task := <- q
-        switch task.(type) {
-        case Combinable:
-            d := task.(Combinable)
-            if c.keyVal == d.key() {
-                c.intVal += d.value()
-            } else {
-                q <- task
-            }
-        default:
-            q <- task
-        }
-    }
-    // after combining, remove the combinable bits
-    q <- &SimpleTask{run: c.runner()}
-}
-
-func (c *SimpleCombiner) key() string { return c.keyVal }
-func (c *SimpleCombiner) value() int { return c.intVal }
-func (c *SimpleCombiner) runner() func() {
-    return func() {
-        log.Printf("Combined %s to val %d", c.key(), c.value())
-    }
 }

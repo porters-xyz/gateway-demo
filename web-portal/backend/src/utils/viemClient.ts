4684770d2357ec0db09848edea968e5daca52f9c
memosys <82053242+memosys@users.noreply.github.com>
2024-05-01 02:30:05 +0530

    save

--- web-portal/backend/src/utils/viemClient.ts
+++ web-portal/backend/src/utils/viemClient.ts
@@ -1,7 +1,12 @@
 import { createPublicClient, http } from 'viem';
-import { optimism } from 'viem/chains';
+import { optimism, sepolia } from 'viem/chains';
 
-export const viemClient = createPublicClient({
+export const viemOPClient = createPublicClient({
   chain: optimism,
-  transport: http(),
+  transport: http('https://optimism-mainnet.rpc.grove.city/v1/' + process.env.RPC_KEY!),
+});
+
+export const viemSepoliaClient = createPublicClient({
+  chain: sepolia,
+  transport: http('https://sepolia.rpc.grove.city/v1/' + process.env.RPC_KEY!),
 });

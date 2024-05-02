000a6957fb848963ca7a2ecdccb99bc68f49142a
memosys <82053242+memosys@users.noreply.github.com>
2024-05-02 18:35:49 +0530

    save

--- web-portal/frontend/components/swap/Redeem.tsx
+++ web-portal/frontend/components/swap/Redeem.tsx
@@ -7,165 +7,212 @@ import { karla } from "@frontend/utils/theme";
 import { portrTokenData } from "@frontend/utils/consts";
 import { chains } from "@frontend/utils/Web3Provider";
 import { useTokenBalance } from "@frontend/utils/hooks";
+import { useChainId, useWriteContract } from "wagmi";
+import web3 from 'web3';
+
+import { abi } from "@frontend/utils/abi";
 
 // Common styles for TextInput and Select components
 const commonStyles = {
-  input: {
-    outline: "none",
-    border: "none",
-    background: "none",
-    fontSize: 24,
-  },
-  label: {
-    color: "#000",
-    marginLeft: 10,
-  },
+    input: {
+        outline: "none",
+        border: "none",
+        background: "none",
+        fontSize: 24,
+    },
+    label: {
+        color: "#000",
+        marginLeft: 10,
+    },
 };
 
 const chainOptions = _.map(chains, "name").filter(
-  (c) => !c.includes("Ethereum"),
+    (c) => !c.includes("Ethereum"),
 );
 
 export default function Redeem() {
-  const [selectedChainId, setSelectedChainId] = useState(10);
+    const [selectedChainId, setSelectedChainId] = useState(10);
+    const { writeContract } = useWriteContract();
 
-  const { data: selectedTokenBalance } = useTokenBalance({
-    token: portrTokenData.address,
-    chainId: portrTokenData?.chainId,
-  });
+    const chainId = useChainId();
+    const { data: selectedTokenBalance } = useTokenBalance({
+        token: portrTokenData.address,
+        chainId: portrTokenData?.chainId,
+    });
 
-  const [redeemValue, setRedeemValue] = useState(0);
+    const [redeemValue, setRedeemValue] = useState(0);
+    const [accountId, setAccountId] = useState();
+    const shouldDisable =
+        !redeemValue ||
+        !(Number(selectedTokenBalance?.formatted) > 0) ||
+        !accountId ||
+        chainId !== selectedChainId;
 
-  return (
-    <Stack p={8} mt={10}>
-      <Select
-        data={chainOptions}
-        defaultValue={_.get(_.find(chains, { id: selectedChainId }), "name")}
-        onChange={(val) => {
-          const selectedChain = _.find(
-            chains,
-            (c) => _.toLower(c.name) === _.toLower(val as string),
-          );
-          setSelectedChainId(selectedChain?.id as number);
-        }}
-        label="Select Network"
-      />
+    const handleRedeem = () =>
+        writeContract({
+            chainId: selectedChainId,
+            address: portrTokenData?.address,
+            abi,
+            functionName: "applyToAccount",
+            args: [web.utils.],
+        });
 
-      <Stack gap={4}>
-        <Flex
-          style={{
-            bg: "white",
-            borderRadius: 10,
-            alignItems: "flex-end",
-            justifyContent: "space-between",
-            border: "1px solid #00000010",
-            padding: 8,
-            backgroundColor: "#F6EEE6",
-          }}
-        >
-          <TextInput
-            placeholder="Enter amount"
-            label="Redeem"
-            type="number"
-            value={redeemValue}
-            onChange={(e) => setRedeemValue(parseFloat(e.target.value))}
-            styles={{
-              ...commonStyles,
-              input: { ...commonStyles.input, fill: "#fff" },
-              error: { marginLeft: 10 },
-            }}
-            error={
-              redeemValue > Number(_.get(selectedTokenBalance, "formatted"))
-                ? "Not enough balance"
-                : undefined
-            }
-          />
-          <Stack>
+    return (
+        <Stack p={8} mt={10}>
+            <Select
+                data={chainOptions}
+                defaultValue={_.get(
+                    _.find(chains, { id: selectedChainId }),
+                    "name",
+                )}
+                onChange={(val) => {
+                    const selectedChain = _.find(
+                        chains,
+                        (c) => _.toLower(c.name) === _.toLower(val as string),
+                    );
+                    setSelectedChainId(selectedChain?.id as number);
+                }}
+                label="Select Network"
+            />
+
+            <Stack gap={4}>
+                <Flex
+                    style={{
+                        bg: "white",
+                        borderRadius: 10,
+                        alignItems: "flex-end",
+                        justifyContent: "space-between",
+                        border: "1px solid #00000010",
+                        padding: 8,
+                        backgroundColor: "#F6EEE6",
+                    }}
+                >
+                    <TextInput
+                        placeholder="Enter amount"
+                        label="Redeem"
+                        type="number"
+                        value={redeemValue}
+                        onChange={(e) =>
+                            setRedeemValue(parseFloat(e.target.value))
+                        }
+                        styles={{
+                            ...commonStyles,
+                            input: { ...commonStyles.input, fill: "#fff" },
+                            error: { marginLeft: 10 },
+                        }}
+                        error={
+                            redeemValue >
+                            Number(_.get(selectedTokenBalance, "formatted"))
+                                ? "Not enough balance"
+                                : undefined
+                        }
+                    />
+                    <Stack>
+                        <Button
+                            style={{
+                                fontFamily: karla.style.fontFamily,
+                                borderRadius: 50,
+                            }}
+                            bg={"blue"}
+                        >
+                            <Image
+                                src={_.get(portrTokenData, "logoURI") as string}
+                                alt={_.get(portrTokenData, "symbol") as string}
+                                width={24}
+                                height={24}
+                                style={{ marginRight: 10, borderRadius: 50 }}
+                            />
+                            {_.get(portrTokenData, "symbol") as string}
+                        </Button>
+                    </Stack>
+                </Flex>
+                <Flex justify="space-between" dir="row" mx={10}>
+                    <Text size="sm">
+                        {`$` +
+                            (isNaN(redeemValue) ? 0 : redeemValue * 5).toFixed(
+                                6,
+                            )}
+                    </Text>
+                    <Flex align={"center"} gap={4}>
+                        <Text size="sm">
+                            {Number(
+                                _.get(selectedTokenBalance, "formatted") ?? 0,
+                            ).toFixed(6)}
+                        </Text>
+
+                        <Text
+                            c="blue"
+                            size="sm"
+                            style={{ fontWeight: "bold", cursor: "pointer" }}
+                            onClick={() =>
+                                setRedeemValue(
+                                    Number(
+                                        _.get(
+                                            selectedTokenBalance,
+                                            "formatted",
+                                            0,
+                                        ),
+                                    ),
+                                )
+                            }
+                        >
+                            max
+                        </Text>
+                    </Flex>
+                </Flex>
+            </Stack>
+            <Flex
+                style={{
+                    bg: "white",
+                    borderRadius: 10,
+                    alignItems: "flex-end",
+                    justifyContent: "space-between",
+                    border: "1px solid #00000010",
+                    paddingLeft: 8,
+                    paddingTop: 8,
+                    backgroundColor: "#F6EEE6",
+                }}
+            >
+                <TextInput
+                    label="Your Account ID"
+                    styles={{
+                        ...commonStyles,
+                        input: { ...commonStyles.input, fill: "#fff" },
+                    }}
+                    value={accountId}
+                    onChange={(e) => setAccountId(e.target.value)}
+                />
+            </Flex>
+            <Flex
+                style={{
+                    bg: "white",
+                    borderRadius: 10,
+                    alignItems: "flex-end",
+                    justifyContent: "space-between",
+                    border: "1px solid #00000010",
+                    padding: 8,
+                    backgroundColor: "#F6EEE6",
+                }}
+            >
+                <TextInput
+                    label="New Balance"
+                    styles={{
+                        ...commonStyles,
+                        input: { ...commonStyles.input, fill: "#fff" },
+                    }}
+                    readOnly
+                />
+            </Flex>
+
             <Button
-              style={{
-                fontFamily: karla.style.fontFamily,
-                borderRadius: 50,
-              }}
-              bg={"blue"}
+                size="lg"
+                c="white"
+                bg={shouldDisable ? "red" : "carrot"}
+                disabled={shouldDisable}
+                onClick={handleRedeem}
             >
-              <Image
-                src={_.get(portrTokenData, "logoURI") as string}
-                alt={_.get(portrTokenData, "symbol") as string}
-                width={24}
-                height={24}
-                style={{ marginRight: 10, borderRadius: 50 }}
-              />
-              {_.get(portrTokenData, "symbol") as string}
+                Redeem
             </Button>
-          </Stack>
-        </Flex>
-        <Flex justify="space-between" dir="row" mx={10}>
-          <Text size="sm">
-            {`$` +
-              Number(_.get(selectedTokenBalance, "formatted") ?? 0).toFixed(6)}
-          </Text>
-          <Flex align={"center"} gap={4}>
-            <Text size="sm">
-              {Number(_.get(selectedTokenBalance, "formatted") ?? 0).toFixed(6)}
-            </Text>
-
-            <Text
-              c="blue"
-              size="sm"
-              style={{ fontWeight: "bold", cursor: "pointer" }}
-              onClick={() =>
-                setRedeemValue(
-                  _.get(selectedTokenBalance, "formatted", 0) as number,
-                )
-              }
-            >
-              max
-            </Text>
-          </Flex>
-        </Flex>
-      </Stack>
-      <Flex
-        style={{
-          bg: "white",
-          borderRadius: 10,
-          alignItems: "flex-end",
-          justifyContent: "space-between",
-          border: "1px solid #00000010",
-          paddingLeft: 8,
-          paddingTop: 8,
-          backgroundColor: "#F6EEE6",
-        }}
-      >
-        <TextInput
-          label="Your Account ID"
-          styles={{
-            ...commonStyles,
-            input: { ...commonStyles.input, fill: "#fff" },
-          }}
-        />
-      </Flex>
-      <Flex
-        style={{
-          bg: "white",
-          borderRadius: 10,
-          alignItems: "flex-end",
-          justifyContent: "space-between",
-          border: "1px solid #00000010",
-          padding: 8,
-          backgroundColor: "#F6EEE6",
-        }}
-      >
-        <TextInput
-          label="New Balance"
-          styles={{
-            ...commonStyles,
-            input: { ...commonStyles.input, fill: "#fff" },
-          }}
-          readOnly
-        />
-      </Flex>
-
-      <Button size="lg">Redeem</Button>
-    </Stack>
-  );
+        </Stack>
+    );
 }

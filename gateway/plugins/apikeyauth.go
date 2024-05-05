0a857150c60558e91b977c47d5aeaaecc5fe61bb
plor <plor@nihil.wtf>
2024-05-05 15:38:13 -0500

    Correctly allows only api key if rule is set

--- gateway/plugins/apikeyauth.go
+++ gateway/plugins/apikeyauth.go
@@ -9,45 +9,77 @@ import (
     "net/http"
 
     "porters/common"
+    "porters/db"
     "porters/proxy"
+    "porters/utils"
 )
 
 type ApiKeyAuth struct {
     ApiKeyName string
 }
 
-func (a ApiKeyAuth) Name() string {
+func (a *ApiKeyAuth) Name() string {
     return "API Key Auth"
 }
 
-func (a ApiKeyAuth) Load() {
+func (a *ApiKeyAuth) Load() {
     // load plugin
     log.Println("loading", a.Name())
 }
 
-func (a ApiKeyAuth) Key() string {
+func (a *ApiKeyAuth) Key() string {
     return "API_KEY_AUTH"
 }
 
-func (a ApiKeyAuth) HandleRequest(req *http.Request) error {
+func (a *ApiKeyAuth) HandleRequest(req *http.Request) error {
+    ctx := req.Context()
     apiKey := req.Header.Get(a.ApiKeyName)
     newCtx := context.WithValue(req.Context(), proxy.AUTH_VAL, apiKey)
 
     if validApiKey(apiKey) {
-        // TODO get app id from ctx
-        // TODO check api key is active
-        //acct, ok := db.LookupAccount(newCtx, apiKey)
-        //if !ok || !db.IsValidAccount(newCtx, acct) {
-            // returning without updating 
-            //return
-        //}
+        appId := proxy.PluckAppId(req)
+        app := &db.App{Id: appId}
+        err := app.Lookup(ctx)
+        if err != nil {
+            return proxy.NewHTTPError(http.StatusBadGateway)
+        }
+
+        hashedKey := utils.Hash(apiKey)
+        rules := a.getRulesForScope(ctx, app)
+        success := (len(rules) == 0)
+
+        for _, rule := range rules {
+            if hashedKey == rule {
+                success = true
+                break
+            }
+        }
+        if success {
+            lifecycle := proxy.SetStageComplete(newCtx, proxy.Auth)
+            newCtx = common.UpdateContext(newCtx, lifecycle)
+            *req = *req.WithContext(newCtx)
+            return nil
+        }
     } else {
-        return proxy.NewHTTPError(http.StatusBadRequest)
+        return proxy.APIKeyInvalidError
     }
-    lifecycle := proxy.SetStageComplete(newCtx, proxy.Auth)
-    newCtx = common.UpdateContext(newCtx, lifecycle)
-    *req = *req.WithContext(newCtx)
-    return nil
+    return proxy.APIKeyInvalidError
+}
+
+func (a *ApiKeyAuth) getRulesForScope(ctx context.Context, app *db.App) []string {
+    apirules := make([]string, 0)
+    rules, err := app.Rules(ctx)
+    if err != nil {
+        log.Println("error getting rules", err)
+    } else {
+        for _, rule := range rules {
+            if rule.RuleType != "secret-key" && !rule.Active {
+                continue
+            }
+            apirules = append(apirules, rule.Value)
+        }
+    }
+    return apirules
 }
 
 // TODO check api key is in valid format to quickly determine errant requests
@@ -60,3 +92,4 @@ func validApiKey(apiKey string) bool {
 func checksumApiKey(apiKey string) bool {
     return true
 }
+

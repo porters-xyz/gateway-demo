e6017d721aa9f1d7db56e86a1ae268f0a5d10ccd
memosys <82053242+memosys@users.noreply.github.com>
2024-05-08 18:21:42 +0530

    remove wagmi wrapper from homepage

--- web-portal/frontend/pages/_app.tsx
+++ web-portal/frontend/pages/_app.tsx
@@ -6,29 +6,36 @@ import { MantineProvider } from "@mantine/core";
 import Web3Provider, { config, projectId } from "@frontend/utils/Web3Provider";
 import { theme } from "@frontend/utils/theme";
 import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { usePathname } from "next/navigation";
 
 createWeb3Modal({
-  siweConfig,
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: false,
-  enableOnramp: false,
+    siweConfig,
+    wagmiConfig: config,
+    projectId,
+    enableAnalytics: false,
+    enableOnramp: false,
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <MantineProvider theme={theme}>
-      <Head>
-        <title>Porters RPC SDK</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-        <link rel="fav icon" href="/favicon.ico" />
-      </Head>
-      <Web3Provider>
-        <Component {...pageProps} />
-      </Web3Provider>
-    </MantineProvider>
-  );
+    const path = usePathname();
+
+    return (
+        <MantineProvider theme={theme}>
+            <Head>
+                <title>Porters RPC Gateway</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+                />
+                <link rel="fav icon" href="/favicon.ico" />
+            </Head>
+            {path === "/" ? (
+                <Component {...pageProps} />
+            ) : (
+                <Web3Provider>
+                    <Component {...pageProps} />
+                </Web3Provider>
+            )}
+        </MantineProvider>
+    );
 }

9a0304ddaef93582b7d03768db46a770d905004f
memosys <82053242+memosys@users.noreply.github.com>
2024-05-11 00:18:47 +0530

    setup prom driver

--- web-portal/backend/src/usage/usage.service.ts
+++ web-portal/backend/src/usage/usage.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrometheusDriver } from 'prometheus-query';
 
 @Injectable()
 export class UsageService {
+  private readonly prom: PrometheusDriver;
+
+  constructor() {
+    this.prom = new PrometheusDriver({
+      endpoint:
+        process.env.PROM_URL ??
+        'https://api.fly.io/prometheus/porters-staging/',
+      baseURL: '/api/v1',
+      headers: {
+        Authorization: String(process.env.PROM_TOKEN),
+      },
+    });
+  }
+
   async getAppUsage(appId: string) {
     return `Usage for app ${appId}`;
   }
+
   async getTenantUsage(tenantId: string) {
     return `Usage for tenant ${tenantId}`;
   }

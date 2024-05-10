c122e38407693563f1817c7041bf1090e6c04935
memosys <82053242+memosys@users.noreply.github.com>
2024-05-11 00:11:45 +0530

    mock endpoint!

--- web-portal/backend/src/usage/usage.controller.ts
+++ web-portal/backend/src/usage/usage.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsageService } from './usage.service';
 
 @Controller('usage')
-export class UsageController {}
+export class UsageController {
+  constructor(private readonly usageService: UsageService) {}
+
+  @Get('/app/:appId')
+  async getAppUsage(@Param('appId') appId: string) {
+    return this.usageService.getAppUsage(appId);
+  }
+
+  @Get('/tenant/:tenantId')
+  async getTenantUsage(@Param('tenantId') tenantId: string) {
+    return this.usageService.getTenantUsage(tenantId);
+  }
+}
--- web-portal/backend/src/usage/usage.service.ts
+++ web-portal/backend/src/usage/usage.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsageService {}
+export class UsageService {
+  async getAppUsage(appId: string) {
+    return `Usage for app ${appId}`;
+  }
+  async getTenantUsage(tenantId: string) {
+    return `Usage for tenant ${tenantId}`;
+  }
+}

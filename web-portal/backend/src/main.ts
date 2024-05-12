32f7411cc59a0db0ebba91eeb95f863ee3b07d99
memosys <82053242+memosys@users.noreply.github.com>
2024-05-12 07:44:12 +0530

    BE networking fix attempt #1

--- web-portal/backend/src/main.ts
+++ web-portal/backend/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  await app.listen(process.env.PORT || 4000, '0.0.0.0');
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();

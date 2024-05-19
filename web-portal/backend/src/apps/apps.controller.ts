0d29b797c863e048503882cdc8c927277b3c18a7
memosys <82053242+memosys@users.noreply.github.com>
2024-05-20 00:59:03 +0530

    clean up!

--- web-portal/backend/src/apps/apps.controller.ts
+++ web-portal/backend/src/apps/apps.controller.ts
@@ -45,25 +45,20 @@ export class AppsController {
 
   @Delete(':appId')
   async deleteApp(
-    @Req() req: Request,
     @Param('appId') appId: string) {
     // @note: This action deletes app for given appId;
-    await this.appsService.verifyAppAccess(req, appId)
     return this.appsService.deleteApp(appId);
   }
 
   @Patch(':appId')
   async updateApp(
-    @Req() req: Request,
     @Param('appId') appId: string, @Body() updateAppDto: any) {
     // @note: This action updates app for given appId;
-    await this.appsService.verifyAppAccess(req, appId)
     return this.appsService.updateApp(appId, updateAppDto);
   }
 
   @Post(':appId/rule')
   async createAppRule(
-    @Req() req: Request,
     @Param('appId') appId: string,
     @Body()
     createAppRuleDto: {
@@ -72,59 +67,48 @@ export class AppsController {
     },
   ) {
     // @note: This action creates app rule given appId;
-    await this.appsService.verifyAppAccess(req, appId)
     return this.appsService.createAppRule(appId, createAppRuleDto.ruleName, createAppRuleDto.data);
   }
 
   @Patch(':appId/rule/:ruleName')
   async updateAppRule(
-    @Req() req: Request,
     @Param('appId') appId: string,
     @Param('ruleName') ruleName: string,
     @Body() updateAppRuleDto: string[],
   ) {
     // @note: This action updates app rule for given appId and ruleName;
-
     return this.appsService.updateAppRule(appId, ruleName, updateAppRuleDto);
   }
 
   @Delete(':appId/rule/:ruleName')
   async deleteAppRule(
-    @Req() req: Request,
     @Param('appId') appId: string,
     @Param('ruleName') ruleName: string,
   ) {
     // @note: This action deletes app rule for given appId and ruleName;
-    await this.appsService.verifyAppAccess(req, appId)
     return this.appsService.deleteAppRule(appId, ruleName);
   }
 
   @Patch(':appId/rules')
   async batchUpdateAppRules(
-    @Req() req: Request,
     @Param('appId') appId: string,
     @Body() updateRulesDto: { ruleName: string; data: string[] },
   ) {
     // @note: This action updates app rules in bulk for given appId;
-    await this.appsService.verifyAppAccess(req, appId)
     return this.appsService.batchUpdateAppRules(appId, updateRulesDto.ruleName, updateRulesDto.data);
   }
 
   @Put(':appId/secret')
   async updateAppSecret(
-    @Req() req: Request,
     @Param('appId') appId: string) {
     // @note: This action updates app secret for given appId;
-    await this.appsService.verifyAppAccess(req, appId)
     return this.appsService.updateSecretKeyRule(appId, 'generate');
   }
 
   @Delete(':appId/secret')
   async deleteAppSecret(
-    @Req() req: Request,
     @Param('appId') appId: string) {
     // @note: This action deletes app secret for given appId;
-    await this.appsService.verifyAppAccess(req, appId)
     return this.appsService.updateSecretKeyRule(appId, 'delete');
   }
 }

f83df9b5561ebc922da8dca23a5e10de02c909cc
plor <plor@nihil.wtf>
2024-05-21 16:55:59 -0500

    Handle a couple issues that were remaining

--- gateway/proxy/error.go
+++ gateway/proxy/error.go
@@ -10,6 +10,7 @@ var FilterBlockError error = fmt.Errorf("filter chain stopped")
 var BalanceExceededError error = fmt.Errorf("balance exceeded: %w", NewHTTPError(http.StatusForbidden))
 var LifecycleIncompleteError error = fmt.Errorf("lifecycle incomplete: %w", NewHTTPError(http.StatusBadGateway))
 var APIKeyInvalidError error = fmt.Errorf("incorrect api key: %w", NewHTTPError(http.StatusUnauthorized))
+var ChainNotSupportedError error = fmt.Errorf("chain not supported: %w", NewHTTPError(http.StatusNotFound))
 
 type HTTPError struct {
     code int
--- gateway/proxy/health.go
+++ gateway/proxy/health.go
@@ -8,7 +8,6 @@ import (
 )
 
 // TODO other healthchecks should be added
-
 func healthHandler(resp http.ResponseWriter, req *http.Request) {
     hc := (&db.Cache{}).Healthcheck()
     resp.Header().Set("Content-Type", "application/json")

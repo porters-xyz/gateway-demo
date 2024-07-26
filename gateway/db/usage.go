d46e74b733c8b121e3330968cf2fb50058daef92
Matthew Scerri <32162885+scermat@users.noreply.github.com>
2024-07-26 21:32:46 +0200

    more logging to try to understand tenant null behaviour

--- gateway/db/usage.go
+++ gateway/db/usage.go
@@ -34,15 +34,16 @@ func NewUsageUpdater(ctx context.Context, status string) *UsageUpdater {
 		log.Error("usage.go > NewUsageUpdater > Failed to get app from context")
 	} else {
 		updater.app = entity.(*App)
-
-		//Ensure Tenant is loaded in context
-		updater.app.Tenant.Lookup(ctx)
 	}
+
+	//Ensure Tenant is loaded in context
+	log.Info("usage.go > NewUsageUpdater > Begin Tenant Lookup for tenant with id", "tenantId", updater.app.Tenant.Id)
+	updater.app.Tenant.Lookup(ctx)
+	log.Info("usage.go > NewUsageUpdater > Finished Tenant Lookup for tenant with id", "tenantId", updater.app.Tenant.Id)
 
 	entity, ok = common.FromContext(ctx, TENANT)
 	if !ok || entity == nil {
-		log.Error("usage.go > NewUsageUpdater > Failed to get tenant from context")
-
+		log.Error("usage.go > NewUsageUpdater > Failed to get tenant from context", "tenantId", updater.app.Tenant.Id, "appId", updater.app.Id, "product", updater.product.Id)
 	} else {
 		tenant := entity.(*Tenant)
 		updater.balancekey = fmt.Sprintf("BALANCE:%s", tenant.Id)

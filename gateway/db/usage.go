66fe08eb59abe6b9c49a0a29db529812abef90d3
Matthew Scerri <32162885+scermat@users.noreply.github.com>
2024-07-26 23:59:48 +0200

    more debugging...

--- gateway/db/usage.go
+++ gateway/db/usage.go
@@ -13,6 +13,7 @@ type UsageUpdater struct {
 	status     string
 	balancekey string
 	app        *App
+	tenant     *Tenant
 	product    *Product
 }
 
@@ -33,7 +34,20 @@ func NewUsageUpdater(ctx context.Context, status string) *UsageUpdater {
 		log.Error("usage.go > NewUsageUpdater > Failed to get app from context")
 	} else {
 		updater.app = entity.(*App)
-		updater.balancekey = fmt.Sprintf("BALANCE:%s", updater.app.Tenant.Id)
+	}
+
+	//Ensure Tenant is loaded in context
+	log.Info("usage.go > NewUsageUpdater > Begin Tenant Lookup for tenant with id", "tenantId", updater.app.Tenant.Id)
+	updater.app.Tenant.Lookup(ctx)
+	log.Info("usage.go > NewUsageUpdater > Finished Tenant Lookup for tenant with id", "tenantId", updater.app.Tenant.Id)
+
+	entity, ok = common.FromContext(ctx, TENANT)
+	if !ok || entity == nil {
+		log.Error("usage.go > NewUsageUpdater > Failed to get tenant from context", "tenantId", updater.app.Tenant.Id, "appId", updater.app.Id, "product", updater.product.Id)
+	} else {
+		tenant := entity.(*Tenant)
+		updater.balancekey = fmt.Sprintf("BALANCE:%s", tenant.Id)
+		updater.tenant = tenant
 	}
 
 	return updater

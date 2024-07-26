d01aa9b7d74275930ce6e4d4fc16724f8ad7c84d
Matthew Scerri <32162885+scermat@users.noreply.github.com>
2024-07-26 22:34:28 +0200

    removed tenant from UsageUpdater

--- gateway/db/usage.go
+++ gateway/db/usage.go
@@ -13,7 +13,6 @@ type UsageUpdater struct {
 	status     string
 	balancekey string
 	app        *App
-	tenant     *Tenant
 	product    *Product
 }
 
@@ -34,43 +33,34 @@ func NewUsageUpdater(ctx context.Context, status string) *UsageUpdater {
 		log.Error("usage.go > NewUsageUpdater > Failed to get app from context")
 	} else {
 		updater.app = entity.(*App)
-	}
-
-	//Ensure Tenant is loaded in context
-	log.Info("usage.go > NewUsageUpdater > Begin Tenant Lookup for tenant with id", "tenantId", updater.app.Tenant.Id)
-	updater.app.Tenant.Lookup(ctx)
-	log.Info("usage.go > NewUsageUpdater > Finished Tenant Lookup for tenant with id", "tenantId", updater.app.Tenant.Id)
-
-	entity, ok = common.FromContext(ctx, TENANT)
-	if !ok || entity == nil {
-		log.Error("usage.go > NewUsageUpdater > Failed to get tenant from context", "tenantId", updater.app.Tenant.Id, "appId", updater.app.Id, "product", updater.product.Id)
-	} else {
-		tenant := entity.(*Tenant)
-		updater.balancekey = fmt.Sprintf("BALANCE:%s", tenant.Id)
-		updater.tenant = tenant
+		updater.balancekey = fmt.Sprintf("BALANCE:%s", updater.app.Tenant.Id)
 	}
 
 	return updater
 }
 
 func (u *UsageUpdater) Run() {
-	if u.app == nil || u.tenant == nil || u.product == nil {
-		log.Error("usage.go > UsageUpdated > Invalid request, usage not reported", "app", u.app, "tenant", u.tenant, "product", u.product)
+	if u.app == nil || u.product == nil {
+		log.Error("usage.go > UsageUpdated > Invalid request, usage not reported", "app", u.app, "product", u.product)
 		return
 	}
+
 	if u.status == "success" {
 		ctx := context.Background()
-		DecrementCounter(ctx, u.balancekey, u.product.Weight)
+		decCounter := DecrementCounter(ctx, u.balancekey, u.product.Weight)
+		log.Info("usage.go > UsageUpdated > Decremented by ", "decCounter", decCounter, "app", u.app)
 
 		use := &Relaytx{
 			AppId:       u.app.Id,
 			ProductName: u.product.Name,
 		}
-		IncrementCounter(ctx, use.Key(), u.product.Weight)
+
+		incCounter := IncrementCounter(ctx, use.Key(), u.product.Weight)
+		log.Info("usage.go > UsageUpdated > Incremented by ", "incCounter", incCounter, "app", u.app)
 	}
-	common.EndpointUsage.WithLabelValues(u.app.HashId(), u.tenant.Id, u.product.Name, u.status).Inc()
+	common.EndpointUsage.WithLabelValues(u.app.HashId(), u.app.Tenant.Id, u.product.Name, u.status).Inc()
 }
 
 func (u *UsageUpdater) Error() string {
-	return fmt.Sprintf("BAL: unable to use relays for %s", u.tenant.Id)
+	return fmt.Sprintf("BAL: unable to use relays for %s", u.app.Tenant.Id)
 }

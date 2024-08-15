8f9a24b5cd4a7de993eb2c934d4497ee64391cae
Matthew Scerri <32162885+scermat@users.noreply.github.com>
2024-08-15 22:03:36 +0200

    included exemption for metrics endpoint for local requests to be allowed through

--- gateway/proxy/kitMetricsKitRouter.go
+++ gateway/proxy/kitMetricsKitRouter.go
@@ -23,20 +23,20 @@ type Machine struct {
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
 	// Commenting due to issue with metrics capturing
 
-	// // Retrieve the expected API key from environment variables
-	// expectedApiKey := os.Getenv("GATEWAY_API_KEY")
+	// Retrieve the expected API key from environment variables
+	expectedApiKey := common.GetConfig(common.GATEWAY_API_KEY)
 
-	// // Get the API key from the request headers
-	// apiKey := r.Header.Get("api-key")
+	// Get the API key from the request headers
+	apiKey := r.Header.Get("api-key")
 
-	// // Validate the API key
-	// if apiKey == "" || apiKey != expectedApiKey {
-	// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
-	// 	return
-	// }
-
-	// Delegate the request to the Prometheus handler
-	promhttp.Handler().ServeHTTP(w, r)
+	// Check if the request is from localhost or the correct API key is provided
+	if strings.HasPrefix(r.RemoteAddr, "127.0.0.1") || strings.HasPrefix(r.RemoteAddr, "::1") || apiKey == expectedApiKey {
+		// Delegate the request to the Prometheus handler
+		promhttp.Handler().ServeHTTP(w, r)
+	} else {
+		// If neither condition is true, return unauthorized
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	}
 }
 
 func qosNodesHandler(w http.ResponseWriter, r *http.Request, proxyToUrl, region string) {

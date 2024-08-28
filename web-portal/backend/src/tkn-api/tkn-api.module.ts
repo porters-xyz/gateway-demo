1fc430e557c5bd9434a85342f243cfab0bddd5fb
Matthew Scerri <32162885+scermat@users.noreply.github.com>
2024-08-28 16:08:16 +0200

    removed tkn api from backend. it is now a separate project

--- web-portal/backend/src/tkn-api/tkn-api.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TknApiController } from './tkn-api.controller';
-
-@Module({
-  controllers: [TknApiController],
-  providers: [],
-})
-export class TknApiModule {}

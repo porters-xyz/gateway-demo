cdfadd8799ec51a1a3210d94b93893e55cb81da3
boucherbox <boucherbox21@gmail.com>
2024-09-23 13:17:04 -0600

    updated logo and spacing and new tab for links

--- web-portal/frontend/components/home/02-Partners.tsx
+++ web-portal/frontend/components/home/02-Partners.tsx
@@ -14,13 +14,13 @@ export default function Partners() {
     return (
         <Container size="md" mt={"xl"}>
             <SectionTitle title="Our Partners" />
-            <Flex wrap='wrap' mt={80} gap={10} rowGap={10} align='center' justify='center'>
+            <Flex wrap='wrap' mt={80} gap={10} rowGap={25} align='center' justify='center'>
                 <Card bg='#F6EEE6' p={16} h={70} ml={20} style={{
                     alignItems: 'center',
                     justifyContent: 'center',
                     borderRadius: 10,
                     cursor: "pointer"
-                }} onClick={() => window.open('https://raidguild.org')}>
+                }} onClick={() => window.open('https://raidguild.org', '_blank')}>
                     <Image
                         src={raidguildLogo.src}
                         alt="Raidguild"
@@ -33,7 +33,7 @@ export default function Partners() {
                     justifyContent: 'center',
                     borderRadius: 10,
                     cursor: "pointer"
-                }} onClick={() => window.open('https://pokt.network')}>
+                }} onClick={() => window.open('https://pokt.network', '_blank')}>
                     <Image
                         src={poktLogo.src}
                         alt="Pokt Network"
@@ -46,7 +46,7 @@ export default function Partners() {
                     justifyContent: 'center',
                     borderRadius: 10,
                     cursor: "pointer"
-                }} onClick={() => window.open('https://taiko.xyz')}>
+                }} onClick={() => window.open('https://taiko.xyz', '_blank')}>
                     <Image
                         src={taikoLogo.src}
                         alt="Taiko Network"
@@ -59,7 +59,7 @@ export default function Partners() {
                     justifyContent: 'center',
                     borderRadius: 10,
                     cursor: "pointer"
-                }} onClick={() => window.open('https://tkn.xyz/')}>
+                }} onClick={() => window.open('https://tkn.xyz/', '_blank')}>
                     <Image
                         src={tknLogo.src}
                         alt="Token Name Service"
@@ -72,7 +72,7 @@ export default function Partners() {
                     justifyContent: 'center',
                     borderRadius: 10,
                     cursor: "pointer"
-                }} onClick={() => window.open('https://bitfinity.network/')}>
+                }} onClick={() => window.open('https://bitfinity.network/', '_blank')}>
                     <Image
                         src={bitfinityLogo.src}
                         alt="Bitfinity Network"
